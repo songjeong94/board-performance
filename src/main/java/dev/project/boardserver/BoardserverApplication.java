d3d6e16770fbaf7c9d0d3513c34fe95371381ebd
songjeong94 <thdwjd7899@naver.com>
2024-02-08 17:01:11 +0900

    first commit

---
+++ src/main/java/dev/project/boardserver/BoardserverApplication.java
@@ -0,0 +1,13 @@
+package dev.project.boardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/project/boardserver/BoardserverApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.project.boardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
